fc38a9208b08a2b33d4b2e8fe08713082860dff2
Sanya Gupta <sanyakvds@gmail.com>
2026-06-17 14:05:41 +0530

    f

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,53 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+import { connectDB } from "@/lib/mongodb";
+import User from "@/models/User";
+
+export async function POST(req: Request) {
+  try {
+    await connectDB();
+
+    const { name, email, password } = await req.json();
+
+    if (!name || !email || !password) {
+      return NextResponse.json(
+        { message: "All fields are required" },
+        { status: 400 }
+      );
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "User Registered Successfully",
+        user,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
